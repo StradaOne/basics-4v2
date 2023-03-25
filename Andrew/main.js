dca7cd16e5e11cf560b9069901829b749d2d0524
Andrew <126349437+Andrew-Gera@users.noreply.github.com>
2023-03-25 14:54:11 +0300

    God help me!

---
+++ Andrew/main.js
@@ -0,0 +1,14 @@
+function calc(operation, a, b) {
+  switch (operation) {
+    case "add":
+      return 1 + 2;
+    case "multi":
+      return 1 * 2;
+    case "substract":
+      return 3 - 2;
+  }
+}
+
+console.log(calc("add", 1, 2)); // 3
+console.log(calc("multi", 1, 2)); // 2
+console.log(calc("subtract", 3, 2)); // 1
