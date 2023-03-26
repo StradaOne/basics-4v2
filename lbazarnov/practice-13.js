4be50fd988975c84a3a13e2949a6a0edbbaeea1a
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-03-26 15:31:11 +0400

    fixed practice #13

--- lbazarnov/practice-13.js
+++ lbazarnov/practice-13.js
@@ -1,15 +1,19 @@
+function isNumber(operand) {
+  return typeof operand === 'number' && !isNaN(operand) && isFinite(operand)
+}
+
 function calc(operatinon, a, b) {
-  switch (operatinon) {
-    case 'add':
-      return a + b
-      break
-    case 'multi':
-      return a * b
-      break
-    case 'subtract':
-      return a - b
-      break
-  default:
-    console.log('Проверьте, вводите ли вы правильные данные');
+  if (isNumber(a) && isNumber(b)) {
+    switch (operatinon) {
+      case 'add':
+        return a + b;
+      case 'multi':
+        return a * b;
+      case 'subtract':
+        return a - b;
+    }
+  }
+  else {
+    return 'Something went wrong'
   }
 }
