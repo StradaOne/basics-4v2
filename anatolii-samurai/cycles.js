7f9d4a21f825686708f67907e50aa4e93fa2eae4
Anatoliy <80347225+anatolii-samurai@users.noreply.github.com>
2023-03-28 18:53:59 +0400

    Update cycles.js

--- anatolii-samurai/cycles.js
+++ anatolii-samurai/cycles.js
@@ -1,18 +1,18 @@
-// let  i = 0;
-// // while(i < 19){
-// //     console.log(i);
-// //     i++;
-// // }
-// do {
-//     console.log(i);
-//     i++;
-// } while(i<=19);
+let  i = 0;
+while(i < 19){
+    console.log(i);
+    i++;
+}
+// 
+do {
+    console.log(i);
+    i++;
+} while(i<=19);
 
-// for (let i = 1;i < 19;i++){
-//     console.log(i);
-// }
-
-
+for (let i = 1;i < 19;i++){
+    console.log(i);
+}
+// 
 
 for(let i = 0; i < 19;i++){
     
