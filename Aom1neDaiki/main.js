059dc83068e18bf2079b8b76a7b6fa134db24489
AomineDaiki <asmerezhko@gmail.com>
2023-03-28 15:00:20 +0300

    object-1

--- Aom1neDaiki/main.js
+++ Aom1neDaiki/main.js
@@ -1,12 +1,15 @@
-function calc(a, b, operation) {
-	switch (operation) {
-		case "add":
-			return a + b;
-		case "multi":
-			return a * b;
-		case "subtract":
-			return a - b;
-		default:
-			return "Ошибка";
-	}
+const phoneBook = {
+	'К-1': '+7-921-32-42',
+	'К': '+7-235-32-42',
+	'К-3': '+7-923-22-65',
+	'К-4': '+7-818-66-66',
+	'К-5': '+7-951-45-15',
 }
+console.log(phoneBook["К-3"]);
+phoneBook["К-6"] = '+7-926-31-16';
+console.log(phoneBook);
+phoneBook.Контакт = '+7-926-41-16';
+console.log(phoneBook);
+delete phoneBook["К-5"];
+console.log(phoneBook);
+console.log(phoneBook["К-4"]);
