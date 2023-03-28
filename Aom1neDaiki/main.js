bed42f9388aa5d180549f21534dc436236845c9c
AomineDaiki <asmerezhko@gmail.com>
2023-03-28 15:12:14 +0300

    Complete-16 object-2

--- Aom1neDaiki/main.js
+++ Aom1neDaiki/main.js
@@ -1,15 +1,9 @@
-const phoneBook = {
-	'К-1': '+7-921-32-42',
-	'К': '+7-235-32-42',
-	'К-3': '+7-923-22-65',
-	'К-4': '+7-818-66-66',
-	'К-5': '+7-951-45-15',
+const test = {
+	'boolean-true': true,
+	'boolean-false': false,
+	'array': [1, 2, 3, 4, 5, 6],
+	'number': 1 + 2 + 3 + 4 + 5 + 6,
+	'null': null,
+	'undefined': undefined,
 }
-console.log(phoneBook["К-3"]);
-phoneBook["К-6"] = '+7-926-31-16';
-console.log(phoneBook);
-phoneBook.Контакт = '+7-926-41-16';
-console.log(phoneBook);
-delete phoneBook["К-5"];
-console.log(phoneBook);
-console.log(phoneBook["К-4"]);
+console.log(test);
