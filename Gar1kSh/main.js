bc118bc7d67d7a58a5e0034cbd6397bee8b882e4
Gar1kSh <skydust63@gmail.com>
2023-03-29 18:55:33 +0300

    create deleteList from task practice #17

--- Gar1kSh/main.js
+++ Gar1kSh/main.js
@@ -1,3 +1,4 @@
+const wrongCellList = console.log('This cell doesn\'t exist in the database');
 const phoneBook = {
     list: {
     "Natali Lopuhova": 291234564,
@@ -10,10 +11,24 @@ const phoneBook = {
       },
     addList(name, number) {
         this.list[name] = number;
+    }, 
+    deleteList(name) {
+        if (this.list[name]) {
+            delete this.list[name];  
+        } else {
+            return wrongCellList;
+        }; 
     }
 };
 
 phoneBook.ShowList();
 phoneBook.addList('Ben Larsson', 63627323);
+phoneBook.deleteList('Peter');
+
 phoneBook.ShowList();
 
+phoneBook.deleteList('Roman');
+
+phoneBook.ShowList();
+phoneBook.addList('Vadim', 222322);
+phoneBook.ShowList();
