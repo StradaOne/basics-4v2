91083abbea56645c49c1990456fec64ea0206365
Marina <128381803+Marishela@users.noreply.github.com>
2023-03-30 15:55:20 +0300

    Practice-13

---
+++ Marina/main.js
@@ -0,0 +1,13 @@
+function calc(a, b, operation) {
+  switch (operation) {
+    case "add":
+      return a + b;
+    case "multi":
+      return a * b;
+    case "subtract":
+      return a - b;
+  }
+}
+console.log(calc(1, 2, "add"));
+console.log(calc(1, 2, "multi"));
+console.log(calc(3, 2, "subtract"));
